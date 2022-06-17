638d22470292844360b89c77596d50fbddfab33f
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-06-17 18:55:51 +0530

    updated

--- api-proj/src/Components/popular.jsx
+++ api-proj/src/Components/popular.jsx
@@ -1,15 +1,28 @@
 
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 function Popular() {
+  const [popular, setPopular] = useState([])
   useEffect(()=>{
     getPopular();
   },[]);
+
+
+  console.log(popular);
   const getPopular = async ()=> {
     const api = await fetch('http://universities.hipolabs.com/search?country=India')
     const data = await api.json();
-    console.log(data);
+    setPopular(data)
   };
-  return <div>popular</div>
+  return <div>
+    {popular.map((info) => {
+      return info.web_pages.map((subInfo) => {
+        return (
+          <p>{subInfo}</p>
+        )
+      })
+      
+    })}
+  </div>
   
 }
 
